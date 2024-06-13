85811651b377900a73b7377376645343340a4914
Mohd Gufran <gufraan2001@gmail.com>
2024-06-13 10:20:11 +0530

    thus

--- minutesConvert.js
+++ minutesConvert.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById("res").hidden = true
 function getSecond() {
     let val = document.getElementById("num").value
@@ -9,16 +8,4 @@ function getSecond() {
         document.getElementById("min").innerHTML = val
         document.getElementById("sec").innerHTML = sec
     }
-=======
-document.getElementById("res").hidden = true
-function getSecond() {
-    let val = document.getElementById("num").value
-    let sec = val * 60
-    console.log(sec)
-    if (sec) {
-        document.getElementById("res").hidden = false
-        document.getElementById("min").innerHTML = val
-        document.getElementById("sec").innerHTML = sec
-    }
->>>>>>> af5d0ba5883be5d215b0bdfd3d721f56bcbebd98
 }
